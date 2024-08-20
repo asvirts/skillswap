df8c951b14498b8e7ae2af159dab48d876481acb
asvirts <asvirts@outlook.com>
2024-08-19 20:43:38 -0500

    refactor: Make name and location fields required in the skills table

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,8 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_08_20_010410) do
   create_table "skills", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
+    t.string "name", null: false
+    t.string "location", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
